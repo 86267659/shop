2d134f7b932aee61846abb1352e98ef6f6b8baaa
LAMP李捷 <86267659@qq.com>
2014-03-31 09:49:28 +0800

    商城项目
    前台界面搭好

---
+++ Index/Conf/config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'APP_STATUS'=>'debug',
+	'APP_GROUP_LIST'=>'Index,Admin',
+	'DEFAULT_GROUP'=>'Index',
+	'DB_TYPE'=>'mysql',
+	'DB_HOST'=>'localhost',
+	'DB_NAME'=>'shop',
+	'DB_USER'=>'root',
+	'DB_PWD'=>'123123',
+	'DB_PREFIX'=>'shop_',
+	'SHOW_ERROR_MSG'=>true,
+	'SHOW_PAGE_TRACE'=>true,
+	'TMPL_FILE_DEPR'=>'_'
+);
+?>
